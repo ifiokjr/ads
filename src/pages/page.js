d600026c2f3c85779c1783708c8c2272acdb242b
ifiokjr <ifiokotung@gmail.com>
2015-05-07 09:35:03 +0000

    add initial tests for page class
    At the moment there's not much here but more to come.

---
+++ src/pages/page.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * Represents a Page class within the VeAds object. 
+ * 
+ * A page is central to the way VeAds functions. 
+ * @constructor
+ * 
+ * @param {object} config - takes in an object with configuration attached
+ */
+
+function Page(config) {
+//   this.type = config.type;
+  this.id = config.id;
+}
+
+
+
+
+
+
+
+
+
+module.exports = Page
--- src/pages/page.spec.js
+++ src/pages/page.spec.js
@@ -1,3 +1,26 @@
 /**
 * Testing the page specification
 */
+
+var Page = require('./page');
+
+var pageObj, config;
+
+beforeEach(function() {  
+  pageObj = new Page(pageConfig);
+});
+
+describe('Page Class', function() {
+  it('should have an id which is the number 2', function() {
+    console.log(pageObj);
+    expect(pageObj.id).to.be.a('number');
+    expect(pageObj.id).to.equal(2);
+  });
+  
+  it('should throw an error if the config object is not passed in', function () {
+    var fn = function() {new Page();};
+    expect(fn).to.throw(Error);
+  });
+  
+});
+
