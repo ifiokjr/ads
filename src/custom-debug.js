ab15d8bb7f3606cfdb4da4f5f0e6195a316294c3
ifiokjr <ifiokotung@gmail.com>
2015-06-23 07:28:22 +0000

    allow chrome extension to work for old versions of the code as well

--- src/custom-debug.js
+++ src/custom-debug.js
@@ -1,2 +1,4 @@
 global.debugVeAds = require( 'debug' ); // Make debug available by default when running tests
 global.debugVeAds('ve')('EVERYTHING SHOULD BE WORKING!!!');
+
+global.debugVeAds.enable('*');
