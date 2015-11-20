45c1852c001e30220c313b408cb9cf79bf672a49
Ifiok Jr <ifiokotung@gmail.com>
2015-11-21 07:06:24 +1100

    fix code truncating alphanumeric values when capturing data
    Changes in response to issue raised on vetechteam.com.
    https://vetechteam.com/t/veads-truncates-values-when-using-alphanumeric-mask/386

--- src/common/masks.js
+++ src/common/masks.js
@@ -20,7 +20,7 @@ masks = {
 
 
   alphanumeric: function( str ) {
-    var alpha = String(str).match(/([\dA-Za-z]{4,25})/);
+    var alpha = String(str).match(/([\dA-Za-z]{4,100})/);
     return type(alpha, 'array') ? alpha[1]: '';
   },
 
